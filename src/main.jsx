06cba9d528cc24f457e18ce46410925c25868943
GitHub Pages Deploy <deploy@github.com>
2026-07-25 04:39:32 +0300

    Fix: Add basename to BrowserRouter for GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/tocollege-admin1/">
       <ToastProvider>
         <App />
         <Toast />
